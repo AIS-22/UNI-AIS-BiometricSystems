653f82ada38e4831253852c907bc86b0d69ce2d0
Michael Hafner <hafneer@gmail.com>
2023-11-05 16:11:39 +0100

    Reorganize source structure

--- src/data_loader/impl/ScutDataloader.py
+++ src/data_loader/impl/ScutDataloader.py
@@ -1,10 +1,10 @@
 from typing import List
 
-from src.AbstractDataLoader import AbstractDataLoader
+from src.data_loader.AbstractDataLoader import AbstractDataLoader
 from src.VeinImageType import VeinImageType
 
 
-class ProtectDataLoader(AbstractDataLoader):
+class ScutDataLoader(AbstractDataLoader):
 
     def __create_dataset(self, transform, use_image_types: List[VeinImageType]) -> None:
         # TODO: Set __train_set and __test_set here!
