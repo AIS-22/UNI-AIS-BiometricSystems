e9e26a2f243a5303441c3558ce0d540044610aef
AleksandarRa <77117953+AleksandarRa@users.noreply.github.com>
2024-01-30 13:55:06 +0100

    Feature/implement create dataset (#9)
    * implement create_dataset
    
    * add cross validation
    
    * change Code to consider the specified dataset. Change filenames
    
    * structure data
    
    * refactor code to consider traintest folders
    
    * Adapt non genuine labels to 1
    
    * implement load_model and evaluateModel. Add types in veinImageType
    
    * Reformat code
    
    * create same structure for every dataset
    
    * add special stuff for mac only
    
    * Add .gitkeep files for models, plots, and results dirs
    remove old gitkeep
    
    * add training and eval lists for easier batch train and eval
    
    * remove mac files, add preprocessing for SCUT and resizing for all gans
    
    * remove src since for some reason if i start it form the cli in the git root folder it does not need it/will not find it when it is there
    
    * close the figures since ther is a warning for too many figures (memory consuption)
    
    * add plots and results
    
    * style: remove Upercase names
    
    * Style: missing whitespace
    
    * Style: fix whitespace
    
    * Style: fix from 'not...in...' to 'not in'
    
    * Style: line to long
    
    * Style: fix continuation line under-indented for visual indent
    
    * update results
    
    ---------
    
    Co-authored-by: Michael Hafner <hafneer@gmail.com>
    Co-authored-by: Moritz Langer <moritz.langer@redbull.com>
    Co-authored-by: Moritz Langer <85168282+m-langer@users.noreply.github.com>

--- src/classifier/impl/ResnetClassifier.py
+++ src/classifier/impl/ResnetClassifier.py
@@ -2,12 +2,14 @@ from typing import Tuple
 
 import numpy as np
 import torch
-from torch import nn, optim
 from sklearn.metrics import precision_recall_fscore_support
+from sklearn.model_selection import KFold
+from torch import nn, optim
 from torch.nn.modules.loss import Module
+from torch.utils.data import DataLoader, Subset
 from torchvision.models.resnet import ResNet
 
-from src.classifier.AbstractClassifier import AbstractClassifier
+from classifier.AbstractClassifier import AbstractClassifier
 
 
 def _get_device() -> str:
@@ -25,35 +27,65 @@ class ResnetClassifier(AbstractClassifier):
                  num_epochs: int,
                  learning_rate: float,
                  model_name: str,
+                 dataset_name: str,
                  model: ResNet,
                  loss_function: Module,
                  num_image_channels: int,
+                 batch_size: int,
+                 folds: int,
                  num_inputs_nodes: Tuple[int, int] = (736, 192),
-                 num_output_nodes: int = 2,
-                 should_save_model: bool = True):
+                 num_output_nodes: int = 2
+                 ) -> object:
 
         self.num_inputs_nodes = num_inputs_nodes
         self.num_output_nodes = num_output_nodes
         self.num_epochs = num_epochs
         self.learning_rate = learning_rate
         self.model_name = model_name
+        self.dataset_name = dataset_name
         self.model = model
         self.criterion = loss_function
         self.optimizer = None
         self.num_image_channels = num_image_channels
-        self.should_save_model = should_save_model
+        self.validation_loss = []
+        self.accuracy = float
+        self.confusion_matrix = []
+        self.folds = folds
+        self.batch_size = batch_size
+        self.device = _get_device()
 
-    def train(self, train_loader, val_loader):
-        device = _get_device()
+    def train(self, dataset):
+        # Create a KFold object
+        kfold = KFold(n_splits=self.folds, shuffle=True, random_state=42)
+
+        # Get the total length of your dataset
+        dataset_length = len(dataset)
 
         # This should change the input conv layer, maybe there is no need for defining the new image sizes
         self.model.conv1 = nn.Conv2d(self.num_image_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
 
-        num_ftrs = self.model.fc.in_features
-        self.model.fc = nn.Linear(num_ftrs, self.num_output_nodes)
-        self.model.to(device)
+        num_input_features = self.model.fc.in_features
+        self.model.fc = nn.Linear(num_input_features, self.num_output_nodes)
+        self.model.to(self.device)
 
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
+
+        # Iterate over the folds
+        for fold, (train_idx, test_idx) in enumerate(kfold.split(range(dataset_length))):
+            print('-----------------------------------------------------------------------')
+            print(f"Fold {fold + 1}:")
+
+            # Create train and validation subsets
+            train_set = Subset(dataset, train_idx)
+            test_set = Subset(dataset, test_idx)
+
+            # Create DataLoader for training and validation
+            train_loader = DataLoader(train_set, batch_size=self.batch_size, shuffle=True)
+            test_loader = DataLoader(test_set, batch_size=self.batch_size, shuffle=False)
+
+            self.train_fold(train_loader, test_loader)
+
+    def train_fold(self, train_loader, test_loader):
 
         losses = np.zeros((self.num_epochs, 2))
         for epoch in range(self.num_epochs):
@@ -61,7 +93,8 @@ class ResnetClassifier(AbstractClassifier):
             self.model.train()
             running_loss = 0.0
             for images, labels in train_loader:
-                images, labels = images.to(device), labels.to(device)
+                labels[labels != 0] = 1
+                images, labels = images.to(self.device), labels.to(self.device)
 
                 self.optimizer.zero_grad()
                 outputs = self.model(images)
@@ -71,7 +104,7 @@ class ResnetClassifier(AbstractClassifier):
 
                 running_loss += loss.item() * images.size(0)
 
-            epoch_loss = running_loss / len(train_loader.dataset.samples)
+            epoch_loss = running_loss / len(train_loader.dataset.indices)
             print(f'Train Epoch: {epoch + 1}, Loss: {epoch_loss:.4f}')
 
             self.model.eval()
@@ -79,8 +112,9 @@ class ResnetClassifier(AbstractClassifier):
             total = 0
             test_loss = 0
             with torch.no_grad():
-                for images, labels in val_loader:
-                    images, labels = images.to(device), labels.to(device)
+                for images, labels in test_loader:
+                    labels[labels != 0] = 1
+                    images, labels = images.to(self.device), labels.to(self.device)
 
                     outputs = self.model(images)
                     _, predicted = torch.max(outputs.data, 1)
@@ -89,33 +123,35 @@ class ResnetClassifier(AbstractClassifier):
                     correct += (predicted == labels).sum().item()
 
             accuracy = correct / total
-            test_loss /= len(val_loader.dataset.samples)
+            test_loss /= len(test_loader.dataset.indices)
             print(f'Validation accuracy: {accuracy:.4f} loss: {test_loss} in epoch: {epoch + 1}')
             losses[epoch, 0] = epoch_loss
             losses[epoch, 1] = test_loss
-        np.save('results/losses_' + self.model_name + '.npy', losses)
-        print('Model trained and losses saved')
 
-        if self.should_save_model:
-            self.save_model()
+        self.validation_loss.append(losses)
 
-    def evaluate(self, val_loader):
-        device = _get_device()
-        self.model.to(device)
+    def evaluate(self, val_set):
+        self.model.to(self.device)
         self.model.eval()
         correct = 0
         total = 0
         all_preds = []
         all_labels = []
+        confusion_matrix = np.zeros((2, 2))
+        val_loader = DataLoader(val_set, batch_size=self.batch_size, shuffle=True)
 
         with torch.no_grad():
             for images, labels in val_loader:
-                images, labels = images.to(device), labels.to(device)
+                labels[labels != 0] = 1
+                images, labels = images.to(self.device), labels.to(self.device)
 
                 outputs = self.model(images)
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)  # Update the total count of processed samples
                 correct += (predicted == labels).sum().item()
+
+                for t, p in zip(labels.view(-1), predicted.view(-1)):
+                    confusion_matrix[t.long(), p.long()] += 1
 
                 all_preds.extend(predicted.cpu().numpy())
                 all_labels.extend(labels.cpu().numpy())
@@ -128,12 +164,39 @@ class ResnetClassifier(AbstractClassifier):
         print(f'Recall: {recall:.4f}')
         print(f'F1-score: {f1_score:.4f}')
 
-        np.save('results/' + self.model_name + '_results.npy', accuracy)
+        self.accuracy = accuracy
+        self.confusion_matrix = confusion_matrix
 
     def save_model(self):
-        torch.save(self.model.state_dict(), "models/cnnParams_" + self.model_name + ".pt")
+        self.model.to('cpu')
+        torch.save(self.model.state_dict(), "models/" + self.dataset_name + "/cnnParams_" + self.model_name + ".pt")
         print("Model saved")
 
     def load_model(self, model_path):
+        num_ftrs = self.model.fc.in_features
+        self.model.fc = nn.Linear(num_ftrs, self.num_output_nodes)
+        self.model.to(self.device)
+
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.learning_rate)
         self.model.load_state_dict(torch.load(model_path))
         print("Model loaded")
+
+    def save_losses(self):
+        losses = np.zeros((self.num_epochs, 2))
+        # Add up losses of each fold run
+        for loss in self.validation_loss:
+            losses += loss
+
+        # Get average
+        losses /= self.folds
+
+        np.save('results/' + self.dataset_name + '/losses_' + self.model_name + '.npy', losses)
+        print('Losses saved')
+
+    def save_val_accuracy(self):
+        np.save('results/' + self.dataset_name + '/accuracy_' + self.model_name + '.npy', self.accuracy)
+        print('Accuracy saved')
+
+    def save_val_confusion_matrix(self):
+        np.save('results/' + self.dataset_name + '/conf_matrix_' + self.model_name + '.npy', self.confusion_matrix)
+        print('Confusion matrix saved')
